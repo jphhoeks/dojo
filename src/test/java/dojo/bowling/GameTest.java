64f3703940b37e6b59204866de2213026329e148
Sjef Hoeks <sjef@hoeks.org>
2015-11-17 20:41:24 +0100

    1e test - uitgebreid

--- src/test/java/dojo/bowling/GameTest.java
+++ src/test/java/dojo/bowling/GameTest.java
@@ -10,6 +10,10 @@ public class GameTest {
   @Test
   public void elkeWorpNulKegels() {
     Game g = new Game();
+    for (int i = 0; i < 20; i++) {
+      g.worp(0);
+    }
+    assertEquals(0, g.score());
   }
 
 }
