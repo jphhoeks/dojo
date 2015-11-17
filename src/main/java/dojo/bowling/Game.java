d1f21fdbf6462d6d0c812bc0f61bc148f1d481b2
Sjef Hoeks <sjef@hoeks.org>
2015-11-17 20:56:11 +0100

    3e test - pass

--- src/main/java/dojo/bowling/Game.java
+++ src/main/java/dojo/bowling/Game.java
@@ -5,13 +5,25 @@ package dojo.bowling;
  */
 public class Game {
 
-  private int score = 0;
+  private int[] worpen = new int[20];
+  private int huidigeWorp = 0;
 
   public void worp(int aantalKegels) {
-    score += aantalKegels;
+    worpen[huidigeWorp++] = aantalKegels;
   }
 
   public int score() {
+    int score = 0;
+    int worpnr = 0;
+    for (int i=0; i < 10; i++) {
+      if (worpen[worpnr] + worpen[worpnr+1] == 10) {
+        score += 10 + worpen[worpnr+2];
+      }
+      else {
+        score += worpen[worpnr] + worpen[worpnr + 1];
+      }
+      worpnr += 2;
+    }
     return score;
   }
 }
