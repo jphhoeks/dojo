f130bec318ced2fff1255ce39c08bc471707d144
Sjef Hoeks <sjef@hoeks.org>
2015-11-17 20:16:39 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Basis voor kata
---
+++ src/test/java/dojo/bowling/GameTest.java
@@ -0,0 +1,12 @@
+package dojo.bowling;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class GameTest {
+
+
+
+}
