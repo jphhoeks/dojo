1805bcc5108166857eb728bba20e359a86ea958f
Sjef Hoeks <sjef@hoeks.org>
2015-11-17 21:14:26 +0100

    5e test - pass

--- src/main/java/dojo/bowling/Game.java
+++ src/main/java/dojo/bowling/Game.java
@@ -16,13 +16,18 @@ public class Game {
     int score = 0;
     int worpnr = 0;
     for (int i=0; i < 10; i++) {
-      if (isSpare(worpnr)) {
+      if (worpen[worpnr] == 10) {
+        score += 10 + worpen[worpnr+1] + worpen[worpnr+2];
+        worpnr += 1;
+      }
+      else if (isSpare(worpnr)) {
         score += 10 + worpen[worpnr+2];
+        worpnr += 2;
       }
       else {
         score += worpen[worpnr] + worpen[worpnr + 1];
+        worpnr += 2;
       }
-      worpnr += 2;
     }
     return score;
   }
