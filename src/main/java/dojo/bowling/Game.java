c90716d476e73604609fe13ea1e92fa6e568350f
Sjef Hoeks <sjef@hoeks.org>
2015-11-17 20:45:58 +0100

    2e test - pass

--- src/main/java/dojo/bowling/Game.java
+++ src/main/java/dojo/bowling/Game.java
@@ -5,11 +5,13 @@ package dojo.bowling;
  */
 public class Game {
 
-  public void worp(int aantalKegels) {
+  private int score = 0;
 
+  public void worp(int aantalKegels) {
+    score += aantalKegels;
   }
 
   public int score() {
-    return 0;
+    return score;
   }
 }
